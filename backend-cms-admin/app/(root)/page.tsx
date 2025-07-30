2b63373e4804c1f55464e1b53cb79f157c7b2234
Mohd Mohibuddin <swe.mohib@gmail.com>
2025-07-30 15:44:24 +0530

    feat: basic setup and shadcn and cleck installation

---
+++ backend-cms-admin/app/(root)/page.tsx
@@ -0,0 +1,30 @@
+import { Button } from "@/components/ui/button";
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <>
+      <header className="flex justify-between text-white border items-center p-4 m-0 gap-4 h-16 bg-blue-500">
+        <nav>hello dashboard ji</nav>
+        <SignedOut>
+          <SignInButton />
+          <SignUpButton>
+            <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+              Sign Up
+            </button>
+          </SignUpButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
+      <Button size={"sm"}>Click Me</Button>
+    </>
+  );
+}
