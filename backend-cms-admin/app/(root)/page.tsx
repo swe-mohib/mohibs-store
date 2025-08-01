8781ba49f6fdf98ed111c389ecadf84f22fc4cc0
Mohd Mohibuddin <swe.mohib@gmail.com>
2025-08-01 06:57:24 +0530

    feat: useStoreModal setup

--- backend-cms-admin/app/(root)/page.tsx
+++ backend-cms-admin/app/(root)/page.tsx
@@ -1,29 +1,22 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
-import {
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+import ClerkAuth from "../(auth)/clerk/page";
+import { useStoreModal } from "@/hooks/useStoreModal";
+import { useEffect } from "react";
 
 export default function Home() {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
   return (
     <>
-      <header className="flex justify-between text-white border items-center p-4 m-0 gap-4 h-16 bg-blue-500">
-        <nav>hello dashboard ji</nav>
-        <SignedOut>
-          <SignInButton />
-          <SignUpButton>
-            <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-              Sign Up
-            </button>
-          </SignUpButton>
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </header>
+      <ClerkAuth />
       <Button size={"sm"}>Click Me</Button>
     </>
   );
--- backend-cms-admin/app/layout.tsx
+++ backend-cms-admin/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ModalProvider } from "@/providers/modelProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <ModalProvider />
           {children}
         </body>
       </html>
