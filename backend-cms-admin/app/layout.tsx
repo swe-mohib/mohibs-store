e89f10f1f666315ea504c794990c26031a6a87d1
Mohd Mohibuddin <swe.mohib@gmail.com>
2025-08-05 22:12:39 +0530

    feat: prisma setup and write create store api

--- backend-cms-admin/app/layout.tsx
+++ backend-cms-admin/app/layout.tsx
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ModalProvider } from "@/providers/modelProvider";
+import { ToastProvider } from "@/providers/toast-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <ToastProvider />
           <ModalProvider />
           {children}
         </body>
