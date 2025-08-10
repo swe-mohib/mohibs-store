5e58508344c3624bb06aa36019e73063af7494e7
Mohd Mohibuddin <swe.mohib@gmail.com>
2025-08-10 22:15:03 +0530

    feat: navbar - switch store

--- backend-cms-admin/app/(dashboard)/[storeId]/layout.tsx
+++ backend-cms-admin/app/(dashboard)/[storeId]/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/navbar";
 import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
@@ -27,7 +28,7 @@ export default async function DashboardLayout({
   }
   return (
     <>
-      <div>TODO:-NAVBAR</div>
+      <Navbar />
       {children}
     </>
   );
