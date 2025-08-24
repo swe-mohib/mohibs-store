03b34552294e08353cda7d3e3284c7bb119add13
Mohd Mohibuddin <swe.mohib@gmail.com>
2025-08-24 21:16:43 +0530

    feat: add order management and UI components

---
+++ backend-cms-admin/app/(dashboard)/[storeId]/(routes)/orders/components/columns.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+
+export type OrderColumn = {
+  id: string;
+  phone: string;
+  address: string;
+  isPaid: boolean;
+  totalPrice: string;
+  products: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export const columns: ColumnDef<OrderColumn>[] = [
+  {
+    accessorKey: "products",
+    header: "Product",
+  },
+  {
+    accessorKey: "phone",
+    header: "Phone",
+  },
+  {
+    accessorKey: "address",
+    header: "Address",
+  },
+  {
+    accessorKey: "totalPrice",
+    header: "Total Price",
+  },
+  {
+    accessorKey: "isPaid",
+    header: "Paid",
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Created At",
+  },
+  {
+    accessorKey: "updatedAt",
+    header: "Updated At",
+  },
+];
---
+++ backend-cms-admin/app/(dashboard)/[storeId]/(routes)/orders/page.tsx
@@ -0,0 +1,51 @@
+import prismadb from "@/lib/prismadb";
+import { OrderClient } from "./components/client";
+import { OrderColumn } from "./components/columns";
+import { format } from "date-fns";
+import { formatter } from "@/lib/utils";
+
+const BillboardsPage = async ({ params }: { params: { storeId: string } }) => {
+  const { storeId } = await params;
+  const orders = await prismadb.order.findMany({
+    where: {
+      storeId,
+    },
+    include: {
+      orderItems: {
+        include: {
+          product: true,
+        },
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const formattedOrders: OrderColumn[] = orders.map((order) => ({
+    id: order.id,
+    phone: order.phone,
+    address: order.address,
+    isPaid: order.isPaid,
+    products: order.orderItems
+      .map((orderItem) => orderItem.product.name)
+      .join(", "),
+    totalPrice: formatter.format(
+      order.orderItems.reduce((total, order) => {
+        return total + Number(order.product.price);
+      }, 0)
+    ),
+    createdAt: format(order.createdAt, "MMMM do, yyyy"),
+    updatedAt: format(order.updatedAt, "MMMM do, yyyy"),
+  }));
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <OrderClient data={formattedOrders} />
+      </div>
+    </div>
+  );
+};
+
+export default BillboardsPage;
