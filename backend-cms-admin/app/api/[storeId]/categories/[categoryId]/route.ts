33cd54c6903e3871bf87be6175ed985a99edea0a
Mohd Mohibuddin <swe.mohib@gmail.com>
2025-09-06 13:16:05 +0530

    feat: include billboard data in category retrieval response

--- backend-cms-admin/app/api/[storeId]/categories/[categoryId]/route.ts
+++ backend-cms-admin/app/api/[storeId]/categories/[categoryId]/route.ts
@@ -19,6 +19,9 @@ export async function GET(
       where: {
         id: categoryId,
       },
+      include: {
+        billboard: true,
+      },
     });
 
     return new NextResponse(JSON.stringify(category), {
