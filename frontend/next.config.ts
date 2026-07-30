7cbd2ed6069c3316987222cfafac337f6cdacee9
Mohd Mohibuddin <swe.mohib@gmail.com>
2026-07-30 13:36:07 +0530

    Add Docker Hub CI/CD, Nginx, Prisma migrate on startup

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   images: {
     remotePatterns: [new URL("https://res.cloudinary.com/df6ou2hiw/**")],
   },
